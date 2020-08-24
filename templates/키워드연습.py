2b98480ffad64d4ca64b8238beb007317bd82263
kst5137 <67016829+kst5137@users.noreply.github.com>
2020-08-24 17:22:30 +0900

    08/24 hide,show,map

--- templates/키워드연습.py
+++ templates/키워드연습.py
@@ -1,4 +1,12 @@
 import simplejson,requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('template.html')
+
 
 url = "https://dapi.kakao.com/v2/local/search/keyword.json?"
 apikey = "9744b0d293119837b040ed5eab047a75"
@@ -9,7 +17,7 @@ for i in r.json()["documents"] :
     print (i["id"])
     print (i["place_name"])
     print (i["road_address_name"])
-
+    print (i["phone"])
 
 
 
